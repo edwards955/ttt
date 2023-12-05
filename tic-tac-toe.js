572dddfc80ef85389b7a33e3ac9fff26b1329b82
edwards955 <getignorantfriday@gmail.com>
2023-12-05 11:54:18 -0500

    Build out game functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -36,8 +36,53 @@ function GameController() {
     currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
   }
 
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${currentPlayer.getPlayerName()}'s turn`)
+  }
+
   const playRound = (row, column) => {
     board.markBoard(row, column, currentPlayer.getPlayerToken());
     changeCurrentPlayer();
+    if (checkForWinner(currentPlayer.getPlayerToken()) === true) {
+      console.log(`${currentPlayer.getPlayerName()} wins!`);
+      return;
+    }
+    printNewRound();
   }
+
+  const checkForWinner = token => {
+    if (board.getBoard()[0][0] === token && board.getBoard()[0][1] === token && board.getBoard()[0][2] === token) {
+      return true;
+    }
+    else if (board.getBoard()[1][0] === token && board.getBoard()[1][1] === token && board.getBoard()[1][2] === token) {
+      return true;
+    }
+    else if (board.getBoard()[2][0] === token && board.getBoard()[2][1] === token && board.getBoard()[2][2] === token) {
+      return true;
+    }
+    else if (board.getBoard()[0][0] === token && board.getBoard()[1][0] === token && board.getBoard()[2][0] === token) {
+      return true;
+    }
+    else if (board.getBoard()[0][1] === token && board.getBoard()[1][1] === token && board.getBoard()[2][1] === token) {
+      return true;
+    }
+    else if (board.getBoard()[0][2] === token && board.getBoard()[1][2] === token && board.getBoard()[2][2] === token) {
+      return true;
+    }
+    else if (board.getBoard()[0][0] === token && board.getBoard()[1][1] === token && board.getBoard()[2][2] === token) {
+      return true;
+    }
+    else if (board.getBoard()[0][2] === token && board.getBoard()[1][1] === token && board.getBoard()[2][0] === token) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  printNewRound();
+
+  return { playRound };
 }
+
+const myGame = GameController();
